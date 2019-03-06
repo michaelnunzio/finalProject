e5111819b23363436c38babbc44249c31b74d282
Vinit Vachhani <vinitvachhani@gmail.com>
2019-03-05 23:07:06 -0500

    build

---
+++ final-app/client/build/precache-manifest.d05d2a7fed92750504ccebc88cd66bb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3958ae4d22900fe900318bac3f00d3cf",
+    "url": "/static/media/techPic.3958ae4d.png"
+  },
+  {
+    "revision": "9940363a611e110e70da55ee3ef1769d",
+    "url": "/static/media/picMill2.9940363a.jpg"
+  },
+  {
+    "revision": "c07dfd5e1d1645b5fcc7005cb13f0f94",
+    "url": "/static/media/LogoMakr_4T9LLy.c07dfd5e.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "156289df334a7a3d0fbe",
+    "url": "/static/js/main.156289df.chunk.js"
+  },
+  {
+    "revision": "70df596ccfce7fab3042",
+    "url": "/static/js/2.70df596c.chunk.js"
+  },
+  {
+    "revision": "156289df334a7a3d0fbe",
+    "url": "/static/css/main.88cea553.chunk.css"
+  },
+  {
+    "revision": "2afdc12cc752ccd3cc3ec6f4640a4487",
+    "url": "/index.html"
+  }
+];
