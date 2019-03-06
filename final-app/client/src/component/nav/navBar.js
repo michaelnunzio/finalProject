938a751093cafe9776cdb9dc9e04e7d7f802a880
james <jjoseph6@gmail.com>
2019-03-06 01:53:22 -0500

    package issues

--- final-app/client/src/component/nav/navBar.js
+++ final-app/client/src/component/nav/navBar.js
@@ -4,6 +4,8 @@ import logo from './images/LogoMakr_4T9LLy.png'; // Tell Webpack this JS file us
 import axios from 'axios';
 
 
+
+
 function handleLogout(){
     axios.get('/logout').then(function(data){
         console.log(data);
@@ -43,22 +45,30 @@ export default class NavBar extends Component{
                         <li className="divider"></li>
                         <li><a href="/register/candidate">Register as Job Seeker</a></li>
                     </ul>
+
                     <ul id="dropdown2" className="dropdown-content">
                         <li><a href="/login/employer">Login as Employer</a></li>
                         <li className="divider"></li>
                         <li><a href="/login/candidate">Login as Job Seeker</a></li>
                     </ul>
+
                     <nav>
                         <div className="nav-wrapper">
-                        <a href="/" className="brand-logo"><img className="responsive-img jHlogo" alt='logo' src={logo} /></a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><a href="/"><i className="material-icons left">home</i>Home</a></li>                    
-                            <li><a className="dropdown-trigger" href="/" data-target="dropdown2" style={otherBtn}><i className="material-icons left">assignment_ind</i>Login<i className="material-icons right">arrow_drop_down</i></a></li>
-                            <li><a href="/" id="logout" onClick={handleLogout} style={styleLogout}><i className="material-icons left">lock</i>Logout</a></li>
-                            <li><a className="dropdown-trigger" href="/" data-target="dropdown1" style={otherBtn}><i className="material-icons left">assignment</i>Register<i className="material-icons right">arrow_drop_down</i></a></li>
+                            <a href="/" className="brand-logo"><img className="responsive-img jHlogo" alt='logo' src={logo} /></a>
 
-                        </ul>
+                                <ul className="right hide-on-med-and-down">
+                                    <li><a href="/"><i className="material-icons left">home</i>Home</a></li>                    
+                                    <li><a className="dropdown-trigger" href="/" data-target="dropdown2" style={otherBtn}><i className="material-icons left">assignment_ind</i>Login<i className="material-icons right">arrow_drop_down</i></a></li>
+                                    <li><a href="/" id="logout" onClick={handleLogout} style={styleLogout}><i className="material-icons left">lock</i>Logout</a></li>
+                                    <li><a className="dropdown-trigger" href="/" data-target="dropdown1" style={otherBtn}><i className="material-icons left">assignment</i>Register<i className="material-icons right">arrow_drop_down</i></a></li>
+                                 </ul>
                         </div>
+
+                            
+
+
+
+
                     </nav>
                 </header>
                 </div>
